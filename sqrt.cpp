903569ed714ceff3f947c274f302a9f4764f9fd3
MikhalIlin <pochta_vechna@bk.ru>
2018-07-16 12:20:16 +0300

    Debug+release

--- sqrt.cpp
+++ sqrt.cpp
@@ -4,16 +4,21 @@
  *  Created on: 16 июл. 2018 г.
  *      Author: user
  */
+//sqrt - использовавние функции sqrt
 #include <iostream>
 #include <cmath>
 int main ()
 {
 	using namespace std;
 	double area;
-	cout << "enter the floor area? in square feet, of your home: ";
+	cout << "enter the floor area in square feet, of your home: ";
 	cin >> area;
 	double side;
 	side = sqrt(area);
+	cout << "this is equivalent of a square " << side
+		 << "feet to the side." << endl;
+	cout << "how fascinating!" << endl;
+	return 0;
 }
 
 
